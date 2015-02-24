9344f9097fe67f1761a1a0b98bae8fb8441dbfef
Francois Faubert <frank@francoisfaubert.com>
2015-02-24 20:53:53 +0000

    improving typecast call

--- db/migrate/20150219150209_convert_artists_ispopular_to_integer.rb
+++ db/migrate/20150219150209_convert_artists_ispopular_to_integer.rb
@@ -1,5 +1,5 @@
 class ConvertArtistsIspopularToInteger < ActiveRecord::Migration
   def change
-    change_column :artists, :is_popular, :integer, :limit => 1
+    change_column :artists, :is_popular, 'integer USING CAST(column_name AS integer)', :limit => 1
   end
 end
